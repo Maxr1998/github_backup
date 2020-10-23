d423e00468e0fcc791a82b388e3991b64c92e506
Maxr1998 <max.rumpf1998@gmail.com>
2020-10-24 00:58:41 +0200

    Group repos by owner, query all pages

--- github-backup.py
+++ github-backup.py
@@ -21,11 +21,18 @@ target_conf = config['target']
 username = auth_conf.get('username') or exit(1)
 token = auth_conf.get('token') or exit(1)
 
-# Query repositories of the supplied user
-response = requests.get(REPO_URL, auth=(username, token), headers=DEFAULT_HEADERS, params={'per_page': 100})
-response_json = json.loads(response.content)
+repos = []
+page = 0
 
-repos = list(map(lambda repo: (repo['name'], repo['full_name'], repo['clone_url']), response_json))
+# Query repositories of the supplied user
+while True:
+    response = requests.get(REPO_URL, auth=(username, token), headers=DEFAULT_HEADERS, params={'per_page': 100, 'page': page})
+    next_repos = json.loads(response.content)
+    if len(next_repos) > 0:
+        repos.extend(list(map(lambda repo: (repo['full_name'], repo['clone_url']), next_repos)))
+        page = page + 1
+    else:
+        break
 
 # Check if target folder exists
 target = target_conf.get('path') or exit(1)
@@ -37,17 +44,17 @@ excludes = (target_conf.get('exclude') or '\n').split('\n')[1:]
 includes = (target_conf.get('include') or '\n').split('\n')[1:]
 
 count = 0
-for path, name, url in repos:
+for name, url in repos:
     if any(fnmatch(name, exclude) for exclude in excludes) and \
             not any(fnmatch(name, include) for include in includes):
         continue
 
-    if not isdir(f'{target}/{path}'):
+    if not isdir(f'{target}/{name}'):
         print(f'→ Cloning {name}\n')
-        subprocess.run(['git', 'clone', '--mirror', url, path], cwd=target)
+        subprocess.run(['git', 'clone', '--mirror', url, name], cwd=target)
     else:
         print(f'→ Updating {name}\n')
-        subprocess.run(['git', 'fetch', '--all', '--prune'], cwd=f'{target}/{path}')
+        subprocess.run(['git', 'fetch', '--all', '--prune'], cwd=f'{target}/{name}')
     print()
     count = count + 1
 
